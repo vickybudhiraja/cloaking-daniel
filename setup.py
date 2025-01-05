abd0b67f4dc607900bab90279e128b5ea7927f3f
vicky <vicky.vigia@gmail.com>
2025-01-05 10:27:02 +0530

    fix: Multiple top-level packages discovered in a flat-layout

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
-    install_requires=REQUIRED_PACKAGES,
+    #install_requires=REQUIRED_PACKAGES,
     python_requires=">=3.10.0",
 )
